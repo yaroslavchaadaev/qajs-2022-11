cf81ceb74cd576dfcb78b7ba34895718e14002ab
Yaroslav Chaadaev <yaroslav.chaadaev@technopark.ru>
2023-08-26 17:51:09 +0300

    added jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
   collectCoverage: false,
   coverageProvider: 'v8',
-
-  // The glob patterns Jest uses to detect test files
   testMatch: ['**/specs/**/*.spec.*'],
-  reporters: ['default', 'jest-allure'],
-  testRunner: 'jest-jasmine2',
-  setupFilesAfterEnv: [
-    'jest-allure/dist/setup'
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters', {
+        publicPath: './jest-html-report',
+        filename: 'report.html'
+      }
+    ]
   ]
 }
